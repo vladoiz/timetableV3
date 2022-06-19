3030ccd7423ec606b8e1eaa707894347f4da72a4
vladoiz <69978160+vladoiz@users.noreply.github.com>
2022-06-19 23:05:26 +0300

    Update calendar.blade.php

--- resources/views/admin/calendar.blade.php
+++ resources/views/admin/calendar.blade.php
@@ -5,7 +5,7 @@
         <div class="col-lg-12">
             <div class="card">
                 <div class="card-header">
-                    Календарь
+                    Расписание
                 </div>
 
                 <div class="card-body">
@@ -32,7 +32,7 @@
                                         @if (is_array($value))
                                             <td rowspan="{{ $value['rowspan'] }}" class="align-middle text-center" style="background-color:#f0f0f0">
                                                 {{ $value['class_name'] }}<br>
-                                                Teacher: {{ $value['teacher_name'] }}
+                                                Расписание: {{ $value['teacher_name'] }}
                                             </td>
                                         @elseif ($value === 1)
                                             <td></td>
